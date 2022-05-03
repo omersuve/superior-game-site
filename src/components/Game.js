a509cfd5fe85cdf9b884ec59434f2e6cb2b6cd5e
omersuve <omer@shopihq.com>
2022-05-03 15:02:04 +0200

    first

---
+++ src/components/Game.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Game extends Component {
+    render() {
+        return (
+            <div className='text-white text-center game'>
+                GAME INFORMATION
+            </div>
+        )
+    }
+}
